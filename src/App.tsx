ec755d0a38800b5da87b415e418e33927f071e99
Your Name <you@example.com>
2022-09-11 14:25:57 +0900

    typeless

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import {useModule, CounterActions, getCounterState} from './module';
+import { useActions, useSelector } from 'typeless';
+import {getCounterLabel, getCount} from './selector';
+
+function Counter() {
+  console.log("Counter");
+  const [count, setCount] = useState(0);
+
+
+  const onClick = () => {
+    setCount(count + 1)
+  };
+
+  return (
+    <div>
+      <div>Count: {count}</div>
+      <button onClick={onClick} >Button</button>
+    </div>
+  );
+}
+
+function TLCounter() {
+  console.log("TLCounter");
+
+  const state = getCounterState.useState();
+  // const state = getCounterState();
+
+  const action = useActions(CounterActions);
+
+  const onClick = () => {
+    action.startCount();
+  };
+
+
+  return (
+    <div>
+      <h3>Typeless Counter</h3>
+      <div>{state.count}</div>
+      <div>{state.isLoading ? "true":"false"}</div>
+      <button onClick={onClick}>Button</button>
+    </div>
+  );
+}
+
+
+function TLSelectorCounter(props: {tag: string}) {
+  console.log(`TLSelectorCounter: ${props.tag}`);
+
+  const countStr = useSelector(getCounterLabel);
+  const count = useSelector(getCount);
+
+  // const countStr = getCounterLabel();
+  // const count = getCount();
+
+  const action = useActions(CounterActions);
+
+  const onClick = () => {
+    action.startCount();
+  };
+
+  return (
+    <div>
+      <h3>Typeless Selector Counter: {props.tag}</h3>
+      <div>{countStr}</div>
+      <div>{count}</div>
+      <button onClick={onClick}>Button</button>
+    </div>
+  );
+}
+
+
+function TLSelectorCounterV2(props: {tag: string}) {
+  console.log(`TLSelectorCounterV2: ${props.tag}`);
+
+  const countStr = useSelector(getCounterLabel);
+  const count = useSelector(getCount);
+
+  // const countStr = getCounterLabel();
+  // const count = getCount();
+
+  const action = useActions(CounterActions);
+
+  const onClick = () => {
+    action.startCount();
+  };
+
+  return (
+    <div>
+      <h3>Typeless Selector Counter: {props.tag}</h3>
+      <div>{countStr}</div>
+      <div>{count}</div>
+      <button onClick={onClick}>Button</button>
+    </div>
+  );
+}
+
+
+function Hoge() {
+
+  console.log("Hoge");
+  return (
+    <div>
+      <h2>Hoge</h2>
+    </div>
+  );
+}
 
 function App() {
+  console.log("App");
+  useModule();
+  const [count, setCount] = useState(0);
+
+
+  const onClick = () => {
+    setCount(count + 1)
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter />
+
+        <TLCounter />
+        <TLSelectorCounter tag="No.1" />
+        <TLSelectorCounter tag="No.2" />
+        <TLSelectorCounterV2 tag="xx" />
+        <Hoge></Hoge>
+        <div>Hoge: {count}</div>
+        <button onClick={onClick} >Button</button>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DefaultTypelessProvider } from 'typeless';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.getElementById('root') as HTMLElement;
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <DefaultTypelessProvider>
+      <App />
+    </DefaultTypelessProvider>
+  </React.StrictMode>,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
